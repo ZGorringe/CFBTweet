c40f38102548007d2c63c88d874e293bb59a5852
ZGorringe <zacgorringe@gmail.com>
2014-10-06 15:07:51 -0600

    still working on routing

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var app = angular.module('cfbTweet', ['ngRoute', 'homeCtrl', 'teamCtrl']);
 app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
-  $routeProvider.when('/',{
-  	templateUrl: '/home.homeTemplate.html',
+  $routeProvider.when('/home',{
+  	templateUrl: 'index.html',
   	controller: 'homeCtrl'
   }).when('/teams/:team', {
   	templateUrl: 'teams/teamTemplate.html',
   	controller: 'teamCtrl'
   }).otherwise('/', {
-  	templateUrl: '/home/homeTemplate.html',
+  	templateUrl: 'index.html',
   	controller: 'homeCtrl'
   });
 
